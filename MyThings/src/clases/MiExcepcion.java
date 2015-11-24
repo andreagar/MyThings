17d336bcd60616c8cbcfeff5fb7652e9cfdbf330
Oscar Bonet <oscar.bonet@opendeusto.es>
2015-11-24 13:31:09 +0100

    MiExcepcion

--- MyThings/src/clases/MiExcepcion.java
+++ MyThings/src/clases/MiExcepcion.java
@@ -1,18 +1,24 @@
 package clases;
 
-public class Excepcion extends Exception{
+public class MiExcepcion extends Exception{
 
 /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-public String Informar(){
+	public String YaExiste(){
 		
 		return "El usuario ya estaba dado de alta.";
 	}
 	
 	public String NoDatos(){
-		return "No hay datos";
+		return "No hay datos.";
+		
 	}
+	
+	public String NoExiste(){
+		return "El usuario no existe.";
+	}
+	
 }
